aa1f72203a212f704c1d58c1a2d6df58c0988c5a
danielhawkesford <daniel.hawkesford@gmail.com>
2022-09-07 14:26:56 +0100

    Added clipping funtionality

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
+/*
+Possible inputs:
+
+- http://www.google.com
+- http://google.com
+- https://www.google.com
+- https://google.com
+- https://maps.google.com
+- www.google.com
+- maps.google.com
+- google.com
+
+Desired outputs:
+
+1. google.com OR maps.google.com (get rid of everything up to the www / before the maps part)
+2. www.google.com or maps.google.com (get rid of everything before the www / maps part)
+
+*/
+
 function clipper(url) {
-    if (url.length > 5) {
-        return 'URL is > 5 characters'
-    } else {
-        return 'URL is < 5 characters'
+    let slicedURL = url
+
+    // Look for :// and get rid of it if it's there
+    const index = slicedURL.indexOf('://');
+    if (index >= 0) {
+        slicedURL = slicedURL.slice(index + 3);
     }
+
+    // console.log(slicedURL);
+    
+    // Then look for www and get rid of it if it's there
+    const index2 = slicedURL.indexOf('www');
+    if (index2 >= 0) {
+        slicedURL = slicedURL.slice(index2 + 4);
+    }
+
+    console.log(slicedURL);
+
+    
+    return slicedURL;
 }
+
+clipper("http://www.google.com")
+clipper("http://google.com")
+clipper("https://www.google.com")
+clipper("https://google.com")
+clipper("https://maps.google.com")
+clipper("www.google.com")
+clipper("maps.google.com")
+clipper("google.com")
 
 export default clipper
