04aea47be7fbf923392c95e44459d687471ee711
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-09-07 16:08:32 +0100

    added functionality for clipping everything after >/< and another one for clipping things after >.com<

--- index.js
+++ index.js
@@ -18,35 +18,45 @@ Desired outputs:
 */
 
 function clipper(url) {
-    let slicedURL = url
+  let slicedURL = url;
 
-    // Look for :// and get rid of it if it's there
-    const index = slicedURL.indexOf('://');
-    if (index >= 0) {
-        slicedURL = slicedURL.slice(index + 3);
-    }
+  // Look for :// and get rid of it if it's there
+  const index = slicedURL.indexOf("://");
+  if (index >= 0) {
+    slicedURL = slicedURL.slice(index + 3);
+  }
 
-    // console.log(slicedURL);
-    
-    // Then look for www and get rid of it if it's there
-    const index2 = slicedURL.indexOf('www');
-    if (index2 >= 0) {
-        slicedURL = slicedURL.slice(index2 + 4);
-    }
+  // console.log(slicedURL);
 
-    console.log(slicedURL);
+  // Then look for www and get rid of it if it's there
+  const index2 = slicedURL.indexOf("www");
+  if (index2 >= 0) {
+    slicedURL = slicedURL.slice(index2 + 4);
+  }
 
-    
-    return slicedURL;
+  const index3 = slicedURL.indexOf("/");
+  if (index3 >= 0) {
+    slicedURL = slicedURL.slice(0, index3);
+  }
+
+  const index4 = slicedURL.lastIndexOf(".");
+  if (index4 >= 0) {
+    slicedURL = slicedURL.slice(0, index4);
+  }
+
+  console.log(slicedURL);
+
+  return slicedURL;
 }
 
-clipper("http://www.google.com")
-clipper("http://google.com")
-clipper("https://www.google.com")
-clipper("https://google.com")
-clipper("https://maps.google.com")
-clipper("www.google.com")
-clipper("maps.google.com")
-clipper("google.com")
+clipper("http://www.google.com/whatever");
+clipper("http://google.com");
+clipper("https://www.google.com");
+clipper("https://google.com");
+clipper("https://maps.google.com");
+clipper("www.google.com");
+clipper("maps.google.com");
+clipper("google.com");
+clipper("https://meet.google.com/bnd-azkm-tmy");
 
-export default clipper
+export default clipper;
