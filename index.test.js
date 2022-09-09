11925a8876ea328ea8a921f1070849a1b4a1aeba
danielhawkesford <daniel.hawkesford@gmail.com>
2022-09-09 01:47:45 +0100

    Added tests for input #2

--- index.test.js
+++ index.test.js
@@ -1,12 +1,12 @@
-import clipper from "./index.js";
+import clipper, { errors } from "./index.js";
 import jest from "jest-mock";
 
 /*
 https://www.google.com/maps/place/Big+Ben/
 [http://, https://, none] + [www, maps, none] + . + google + . + [.com, .co.uk] + [/maps/place/Big+Ben, none]
 
-- http://www.google.com/maps/place/Big+Ben/
-- http://www.google.com
+- ✅ http://www.google.com/maps/place/Big+Ben/
+- ✅ http://www.google.com
 - http://www.google.co.uk/maps/place/Big+Ben/
 - http://www.google.co.uk
 - http://maps.google.com/maps/place/Big+Ben/
@@ -50,28 +50,60 @@ describe("http://www.google.com/maps/place/Big+Ben/", () => {
   const url = "http://www.google.com/maps/place/Big+Ben/";
   console.log = jest.fn();
 
-  test('', () => {
+  test(`clipper(url, "none") should return "http://www.google.com/maps/place/Big+Ben/"`, () => {
     expect(clipper(url, "none")).toBe(url);
   });
 
-  test('', () => {
+  test(`clipper(url, "shorten") should return "www.google.com/maps/place/Big+Ben/"`, () => {
     expect(clipper(url, "shorten")).toBe("www.google.com/maps/place/Big+Ben/");
   });
 
-  test('', () => {
+  test(`clipper(url, "remove") should return "google.com/maps/place/Big+Ben/"`, () => {
     expect(clipper(url, "remove")).toBe("google.com/maps/place/Big+Ben/");
   });
 
-  test('', () => {
+  test(`clipper(url, "none", "shorten") should return "http://www.google.com"`, () => {
     expect(clipper(url, "none", "shorten")).toBe("http://www.google.com");
   });
 
-  test('', () => {
+  test(`clipper(url, "shorten", "shorten") should return "www.google.com"`, () => {
     expect(clipper(url, "shorten", "shorten")).toBe("www.google.com");
   });
 
-  test('', () => {
+  test(`clipper(url, "remove", "shorten") should return "google.com"`, () => {
     expect(clipper(url, "remove", "shorten")).toBe("google.com");
+  });
+});
+
+describe("http://www.google.com", () => {
+  const url = "http://www.google.com";
+  console.log = jest.fn();
+
+  test(`clipper(url, "none") should return "http://www.google.com"`, () => {
+    expect(clipper(url, "none")).toBe(url);
+  });
+
+  test(`clipper(url, "shorten") should return "www.google.com"`, () => {
+    expect(clipper(url, "shorten")).toBe("www.google.com");
+  });
+  
+  test(`clipper(url, "remove") should return "google.com"`, () => {
+    expect(clipper(url, "remove")).toBe("google.com");
+  });
+  
+  test(`clipper(url, "none", "shorten") should return "http://www.google.com" and log the "directories" error message`, () => {
+    expect(clipper(url, "none", "shorten")).toBe("http://www.google.com");
+    expect(console.log).toHaveBeenCalledWith(errors.directories);
+  });
+  
+  test(`clipper(url, "shorten", "shorten") should return "www.google.com" and log the "directories" error message`, () => {
+    expect(clipper(url, "shorten", "shorten")).toBe("www.google.com");
+    expect(console.log).toHaveBeenCalledWith(errors.directories);
+  });
+  
+  test(`clipper(url, "remove", "shorten") should return "google.com" and log the "directories" error message`, () => {
+    expect(clipper(url, "remove", "shorten")).toBe("google.com");
+    expect(console.log).toHaveBeenCalledWith(errors.directories);
   });
 });
 
