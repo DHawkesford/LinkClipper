74cda9ec1275703d91ab100a3f33e04fb6ce5d14
danielhawkesford <daniel.hawkesford@gmail.com>
2022-09-09 01:35:25 +0100

    Added a list of possible input URLs and added a test suite

--- index.test.js
+++ index.test.js
@@ -1,6 +1,80 @@
 import clipper from "./index.js";
 import jest from "jest-mock";
 
+/*
+https://www.google.com/maps/place/Big+Ben/
+[http://, https://, none] + [www, maps, none] + . + google + . + [.com, .co.uk] + [/maps/place/Big+Ben, none]
+
+- http://www.google.com/maps/place/Big+Ben/
+- http://www.google.com
+- http://www.google.co.uk/maps/place/Big+Ben/
+- http://www.google.co.uk
+- http://maps.google.com/maps/place/Big+Ben/
+- http://maps.google.com
+- http://maps.google.co.uk/maps/place/Big+Ben/
+- http://maps.google.co.uk
+- http://google.com/maps/place/Big+Ben/
+- http://google.com
+- http://google.co.uk/maps/place/Big+Ben/
+- http://google.co.uk
+- https://www.google.com/maps/place/Big+Ben/
+- https://www.google.com
+- https://www.google.co.uk/maps/place/Big+Ben/
+- https://www.google.co.uk
+- https://maps.google.com/maps/place/Big+Ben/
+- https://maps.google.com
+- https://maps.google.co.uk/maps/place/Big+Ben/
+- https://maps.google.co.uk
+- https://google.com/maps/place/Big+Ben/
+- https://google.com
+- https://google.co.uk/maps/place/Big+Ben/
+- https://google.co.uk
+- www.google.com/maps/place/Big+Ben/
+- www.google.com
+- www.google.co.uk/maps/place/Big+Ben/
+- www.google.co.uk
+- maps.google.com/maps/place/Big+Ben/
+- maps.google.com
+- maps.google.co.uk/maps/place/Big+Ben/
+- maps.google.co.uk
+- google.com/maps/place/Big+Ben/
+- google.com
+- google.co.uk/maps/place/Big+Ben/
+- google.co.uk
+
+clipper(none), clipper(shorten), clipper(remove)
+clipper(none, shorten), clipper(shorten, shorten), clipper(remove, shorten)
+*/
+
+describe("http://www.google.com/maps/place/Big+Ben/", () => {
+  const url = "http://www.google.com/maps/place/Big+Ben/";
+  console.log = jest.fn();
+
+  test('', () => {
+    expect(clipper(url, "none")).toBe(url);
+  });
+
+  test('', () => {
+    expect(clipper(url, "shorten")).toBe("www.google.com/maps/place/Big+Ben/");
+  });
+
+  test('', () => {
+    expect(clipper(url, "remove")).toBe("google.com/maps/place/Big+Ben/");
+  });
+
+  test('', () => {
+    expect(clipper(url, "none", "shorten")).toBe("http://www.google.com");
+  });
+
+  test('', () => {
+    expect(clipper(url, "shorten", "shorten")).toBe("www.google.com");
+  });
+
+  test('', () => {
+    expect(clipper(url, "remove", "shorten")).toBe("google.com");
+  });
+});
+
 describe("maps.google.co.uk/whatever", () => {
   const url = "maps.google.co.uk/whatever";
   console.log = jest.fn();
