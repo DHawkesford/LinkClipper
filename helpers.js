3c3fcdce2c5dc0f0eaaf9925d1a1d703a18a4cb5
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-09-29 12:25:42 +0100

    Added to UpperCase in case user enters argumentsi n wrong casing

--- helpers.js
+++ helpers.js
@@ -1,55 +1,57 @@
 export const errors = {
-    protocol: `ERROR: It looks like your link does not include "https://" nor "http://"`,
-    www: `ERROR: It looks like your link does not include "www"`,
-    path: `ERROR: It looks like your link does not include a path`,
+  protocol: `ERROR: It looks like your link does not include "https://" nor "http://"`,
+  www: `ERROR: It looks like your link does not include "www"`,
+  path: `ERROR: It looks like your link does not include a path`,
 };
 
-
 export function clipStart(url, start) {
-    const indicesStart = {
-        h: url.indexOf("://"),
-        w: url.indexOf("www"),
-    };
+  const indicesStart = {
+    h: url.indexOf("://"),
+    w: url.indexOf("www"),
+  };
 
-    var slicedURL = url;
-
-    try {
-        if (start === "shorten" && indicesStart.h >= 0) {
-            // If there's a protocol
-            slicedURL = url.slice(indicesStart.h + 3);
-        } else if (start === "remove" && indicesStart.w >= 0) {
-            // If there's a 'www'
-            slicedURL = url.slice(indicesStart.w + 4);
-        } 
-        else if (start === "remove" && indicesStart.w < 0 && indicesStart.h >= 0) {
-            // If there's a protocol but no 'www'
-            slicedURL = url.slice(indicesStart.h + 3);
-        }
-        // If there's no protocol:
-        else if (start === "shorten" && indicesStart.h < 0) throw errors.protocol;
-    } catch (err) {
-        console.error(err);
+  var slicedURL = url;
+  var start = start.toUpperCase();
+  try {
+    if (start === "SHORTEN" && indicesStart.h >= 0) {
+      // If there's a protocol
+      slicedURL = url.slice(indicesStart.h + 3);
+    } else if (start === "REMOVE" && indicesStart.w >= 0) {
+      // If there's a 'www'
+      slicedURL = url.slice(indicesStart.w + 4);
+    } else if (
+      start === "REMOVE" &&
+      indicesStart.w < 0 &&
+      indicesStart.h >= 0
+    ) {
+      // If there's a protocol but no 'www'
+      slicedURL = url.slice(indicesStart.h + 3);
     }
+    // If there's no protocol:
+    else if (start === "SHORTEN" && indicesStart.h < 0) throw errors.protocol;
+  } catch (err) {
+    console.error(err);
+  }
 
-    return slicedURL;
+  return slicedURL;
 }
 
 export function clipEnd(url, end) {
-    var slicedURL = url;
+  var slicedURL = url;
+  var end = end.toUpperCase();
+  try {
+    let indexSlash = url.indexOf("/", slicedURL.indexOf("://") + 3);
 
-    try {
-        let indexSlash = url.indexOf("/", slicedURL.indexOf("://") + 3);
-      
-        if (url.indexOf("://") < 0) {
-          indexSlash = url.indexOf("/");
-        }
-      
-        if (end === "shorten" && indexSlash >= 0) {
-          slicedURL = url.slice(0, indexSlash);
-        } else if (end === "shorten" && indexSlash < 0) throw errors.path;
-    } catch (err) {
-        console.error(err);
+    if (url.indexOf("://") < 0) {
+      indexSlash = url.indexOf("/");
     }
 
-    return slicedURL;
+    if (end === "SHORTEN" && indexSlash >= 0) {
+      slicedURL = url.slice(0, indexSlash);
+    } else if (end === "SHORTEN" && indexSlash < 0) throw errors.path;
+  } catch (err) {
+    console.error(err);
+  }
+
+  return slicedURL;
 }
--- index.js
+++ index.js
@@ -2,14 +2,15 @@ import { clipStart, clipEnd } from "./helpers.js";
 
 function clipper(url, start, end) {
   try {
-    if (typeof url !== "string") throw 'URL needs to be a string.';
+    if (typeof url !== "string") throw "URL needs to be a string.";
 
-    const URLSlicedStart = clipStart(url, start)
-    const URLSlicedEnd = clipEnd(URLSlicedStart, end)
+    const URLSlicedStart = clipStart(url, start);
+    const URLSlicedEnd = end ? clipEnd(URLSlicedStart, end) : URLSlicedStart;
+
     return URLSlicedEnd;
   } catch (e) {
-    console.error(e)
-  };
+    console.error(e);
+  }
 }
 
 export default clipper;
